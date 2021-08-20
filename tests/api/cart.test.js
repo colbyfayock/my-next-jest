667b1bdbe550c67bb811a982771b8a0cefb9e4d1
colbyfayock <fayock@gmail.com>
2021-08-20 01:11:53 -0400

    Step 3: creating a test for a serverless function

--- tests/api/cart.test.js
+++ tests/api/cart.test.js
@@ -1,5 +1,38 @@
+import cart from '../../pages/api/cart';
+
 test('calculates order total', () => {
-  const price = 5.00;
-  const quantity = 3;
-  expect(price * quantity).toEqual(10);
+  const req = {
+    body: JSON.stringify({
+      discount: .2,
+      tax: .06,
+      items: [
+        {
+          id: 1,
+          price: 19.99,
+          quantity: 2
+        },
+        {
+          id: 2,
+          price: 43.49,
+          quantity: 1
+        }
+      ]
+    })
+  }
+
+  const json = jest.fn();
+
+  const status = jest.fn(() => {
+    return {
+      json
+    }
+  })
+
+  const res = {
+    status
+  }
+
+  cart(req, res);
+
+  expect(json.mock.calls[0][0].subtotal).toEqual(83.47);
 });
